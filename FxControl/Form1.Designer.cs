802b22dd391df26f222fcb63824634b25fd48c7c
Abdulkadiraktas <abdulkadir.aktas@hotmail.com>
2019-06-01 16:17:49 +0300

    FXServerController Release
    FXServerController Release

---
+++ FxControl/Form1.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace FxControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.txtconfig = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.txtfx = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(446, 363);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox2);
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(438, 337);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Control";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button8);
+            this.groupBox2.Controls.Add(this.button7);
+            this.groupBox2.Controls.Add(this.listBox1);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.dateTimePicker1);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(3, 103);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(432, 231);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Otomation";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(100, 19);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(112, 186);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(6, 45);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 38);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Add List";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker1.Location = new System.Drawing.Point(6, 19);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.ShowUpDown = true;
+            this.dateTimePicker1.Size = new System.Drawing.Size(88, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(432, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Server Panel";
+            // 
+            // button3
+            // 
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Coral;
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Crimson;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(306, 19);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 62);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Restart";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Coral;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Crimson;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(149, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 62);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Coral;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Crimson;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(6, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 62);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.groupBox4);
+            this.tabPage2.Controls.Add(this.groupBox3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(438, 419);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Setting";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.button6);
+            this.groupBox4.Controls.Add(this.txtconfig);
+            this.groupBox4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox4.Location = new System.Drawing.Point(3, 103);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(432, 100);
+            this.groupBox4.TabIndex = 1;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Config File Location";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(6, 45);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(416, 36);
+            this.button6.TabIndex = 1;
+            this.button6.Text = "Change Location";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // txtconfig
+            // 
+            this.txtconfig.Enabled = false;
+            this.txtconfig.Location = new System.Drawing.Point(6, 19);
+            this.txtconfig.Name = "txtconfig";
+            this.txtconfig.Size = new System.Drawing.Size(416, 20);
+            this.txtconfig.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button5);
+            this.groupBox3.Controls.Add(this.txtfx);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox3.Location = new System.Drawing.Point(3, 3);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(432, 100);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Server Location";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(6, 45);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(416, 36);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "Change Location";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // txtfx
+            // 
+            this.txtfx.Enabled = false;
+            this.txtfx.Location = new System.Drawing.Point(6, 19);
+            this.txtfx.Name = "txtfx";
+            this.txtfx.Size = new System.Drawing.Size(416, 20);
+            this.txtfx.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(6, 105);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(88, 38);
+            this.button7.TabIndex = 3;
+            this.button7.Text = "Delete Selected";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(6, 167);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(88, 38);
+            this.button8.TabIndex = 4;
+            this.button8.Text = "Clear List";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.richTextBox1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(522, 440);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Logs";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(516, 434);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar1.Location = new System.Drawing.Point(0, 363);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(446, 36);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 399);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FX Controller";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox txtfx;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox txtconfig;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ FxControl/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FxControl
+{
+    public partial class Form1 : Form
+    {
+        private Process _process;
+        private Boolean _isFivemServerRunning, oyunbasladı;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void fxselectlocation()
+        {
+            if (FxControl.Properties.Settings.Default.fxlocation == null)
+            {
+                OpenFileDialog fx = new OpenFileDialog();
+                fx.Title = "Please select FXServer.exe";
+                fx.FileName = "FXServer.exe";
+                fx.Filter = "FXServer|FXServer.exe";
+                if (fx.ShowDialog() == DialogResult.OK)
+                {
+                    FxControl.Properties.Settings.Default.fxlocation = fx.FileName;
+                    FxControl.Properties.Settings.Default.Save();
+                    txtfx.Text = FxControl.Properties.Settings.Default.fxlocation;
+                }
+                else
+                {
+                    this.Close();
+                }
+            }
+            else
+            {
+                txtfx.Text = FxControl.Properties.Settings.Default.fxlocation;
+            }
+        }
+        private void configselectlocation()
+        {
+            if (FxControl.Properties.Settings.Default.config == null)
+            {
+                OpenFileDialog conf = new OpenFileDialog();
+                conf.Title = "Please select server.cfg";
+                conf.FileName = "server.cfg";
+                conf.Filter = "Config|server.cfg";
+                if (conf.ShowDialog() == DialogResult.OK)
+                {
+                    FxControl.Properties.Settings.Default.config = conf.FileName;
+                    FxControl.Properties.Settings.Default.Save();
+                    txtconfig.Text = FxControl.Properties.Settings.Default.config;
+                }
+                else
+                {
+                    this.Close();
+                }
+            }
+            else
+            {
+                txtconfig.Text = FxControl.Properties.Settings.Default.config;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            fxselectlocation();
+            configselectlocation();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(dateTimePicker1.Value.ToString("HH:mm:ss"));
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        { 
+            if (!oyunbasladı)
+            {
+                button1.BackColor = Color.Transparent;
+            }
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                if (DateTime.Now.ToString("HH:mm:ss") == listBox1.Items[i].ToString())
+                {
+                    listBox1.Items.Remove(listBox1.Items[i]);
+                    timer1.Stop();
+                    OnRestart();
+                }
+            }
+
+            if (_isFivemServerRunning)
+            {
+                button1.Enabled = false;
+                button2.Enabled = true;
+                button3.Enabled = true;
+                button5.Enabled = false;
+                button6.Enabled = false;
+                CheckIfCrashed();
+            }
+            else
+            {
+                button1.Enabled = true;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button5.Enabled = true;
+                button6.Enabled = true;
+                progressBar1.Value = 0;
+            }
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            FxControl.Properties.Settings.Default.fxlocation = null;
+            FxControl.Properties.Settings.Default.Save();
+            fxselectlocation();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            FxControl.Properties.Settings.Default.config = null;
+            FxControl.Properties.Settings.Default.Save();
+            configselectlocation();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StartServer();
+            progressBar1.Maximum = 100;
+
+
+        }
+        private void StopServer()
+        {
+            _isFivemServerRunning = false; 
+            _process.Kill();
+        }
+
+        private void OnRestart()
+        {
+            StopServer();
+            Thread.Sleep(5000);
+            StartServer();
+        }
+        public void CheckIfCrashed()
+        {
+            if (!_isFivemServerRunning) return; 
+
+            if (!IsProcessRunning())
+            { 
+                StartServer();
+                return;
+            } 
+        }
+        private bool IsProcessRunning()
+        {
+            if (this._process == null)
+            {
+                return false;
+            }
+
+            try
+            {
+                Process.GetProcessById(this._process.Id);
+            }
+            catch (ArgumentException)
+            {
+                return false;
+            }
+
+            return true;
+        }
+        public void StartServer()
+        {
+            oyunbasladı = false;
+            string serverPath = FxControl.Properties.Settings.Default.config.Replace("server.cfg", "");
+            System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(@serverPath + "cache");
+            if (dir.Exists)
+            {
+                try
+                {
+                    dir.Delete(true);
+                }
+                catch (Exception)
+                {
+                     
+                }
+                Thread.Sleep(3000);
+            }
+            _process = new Process(); 
+
+            var startInfo = new ProcessStartInfo
+            {
+                WindowStyle = ProcessWindowStyle.Hidden,
+                FileName = FxControl.Properties.Settings.Default.fxlocation,       // +set citizen_dir C:/FiveM/FXServer.exe
+                Arguments = "+set citizen_dir " + FxControl.Properties.Settings.Default.fxlocation.Replace("FXServer.exe", "") + // C:/FiveM
+                         "/citizen/ +exec server.cfg",         // /citizen/ +exec server.cfg
+                WorkingDirectory = FxControl.Properties.Settings.Default.config.Replace("\\server.cfg", "")                    // C:/FiveM/cfx-server-data-master
+            };
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardOutput = true;
+            _process.StartInfo = startInfo;
+            // string date = DateTime.Now.ToString("yyyy_MM_dd_HH_mm"); 
+            _process.OutputDataReceived += CaptureOutput;
+            _process.Start();
+            _process.BeginOutputReadLine();
+            _isFivemServerRunning = true;
+        } 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            oyunbasladı = false;
+            richTextBox1.Text = "";
+            StopServer();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            OnRestart();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItems.Count > 0)
+            {
+                for (int i = 0; i < listBox1.SelectedItems.Count; i++)
+                {
+                    listBox1.Items.Remove(listBox1.SelectedItems[i]);
+                }
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void CaptureOutput(object sender, DataReceivedEventArgs e)
+        {
+            if (e.Data != null)
+            {
+                richTextBox1.Text = e.Data + Environment.NewLine + richTextBox1.Text;
+                if (e.Data== "Server license key authentication succeeded. Welcome!")
+                {
+                    oyunbasladı = true;
+                    progressBar1.Value = progressBar1.Maximum;
+                    
+                    button1.BackColor = Color.Green;
+                }
+                if (!oyunbasladı)
+                {
+                    if (progressBar1.Value < progressBar1.Maximum)
+                    {
+                        progressBar1.Value += 1;
+                    }
+                    else
+                    {
+                        progressBar1.Maximum += 100;
+                    }
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _process.Kill();
+        }
+    }
+}
